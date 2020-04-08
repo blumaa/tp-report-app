d0bcc75ba64fe836496e0c9a596069976e7cb9e1
blumaa <blumaa@gmail.com>
2020-04-08 14:53:20 +0200

    switched proxy

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use('/graphql', graphqlHTTP({
 // );
 
 function main() {
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 8000;
   const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-om3us.mongodb.net/test?retryWrites=true&w=majority`;
   // console.log(uri)
   mongoose
